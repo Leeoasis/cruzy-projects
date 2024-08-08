dfedd68e5066d37d0b27a57035a67e4a2bd8471c
Leslie <Kooliesk@gmail.com>
2024-08-08 14:56:12 +0200

    add home content and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
         nosi: ["Nosifer", "sans-serif"],
       },
       colors: {
-        'dark-blue': '#f7fafc',
+        'olive-700': '#4A5D23',
+        'olive-600': '#5A6F2F',
+        'olive-500': '#6A7F3D',
+      },
+      backgroundImage: {
+        'olive-green-gradient': 'linear-gradient(to right, #3A5A40, #2F4F2F)',
       },
     },
   },
